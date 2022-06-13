38bae42ea97cb903ba899ddb3e5c367d7b642133
Edenys Deniz González <holofist1@gmail.com>
2022-06-13 03:09:34 -0400

    feat: add new project

--- src/conf.js
+++ src/conf.js
@@ -1,12 +1,24 @@
 import portfolioMedia from "./media/preview/portfolio"
+import gatoRatonMedia from "./media/preview/gato_raton"
 export const projects = [
     {
         title: "Edenys's Portfolio",
         description: "Here is the project for this portfolio in case you have any questions or simply do you want to see how it works.",
         media: portfolioMedia,
         github: {
+            src: "https://github.com/edenysd/edenysd.github.io",
             owner: "edenysd",
             repo: "edenysd.github.io"
+        }
+    },
+    {
+        title: "El Gato y el Raton",
+        description: "A beautiful game with some interesting things. The entire game is rendered on a single canvas using good programming practices to control game state and transicions. The only utilities used are the android canvas and the system event provider.",
+        media: gatoRatonMedia,
+        github: {
+            src: "https://github.com/edenysd/El-Gato-y-el-Raton",
+            owner: "edenysd",
+            repo: "El-Gato-y-el-Raton"
         }
     }
 ]
---
+++ src/media/preview/gato_raton/index.js
@@ -0,0 +1,7 @@
+import preview from "./preview.jpg"
+import gato_bravo from "./gato_bravo.png"
+import logo from "./logo.png"
+import splash from "./splash.png"
+
+const resources = [preview, gato_bravo, logo, splash]
+export default resources
