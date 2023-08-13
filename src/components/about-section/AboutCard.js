3922e17547718ec53355a4ddcb98f2dfcb1139ff
Edenys Deniz González <holofist1@gmail.com>
2023-08-12 23:29:21 -0400

    content: reestructure text idea focusing in simplicity

--- src/components/about-section/AboutCard.js
+++ src/components/about-section/AboutCard.js
@@ -1,26 +1,30 @@
-import profilePhoto from "../../media/sections/about-section/profile-photo.jpg"
-
+import profilePhoto from "../../media/sections/about-section/profile-photo.jpg";
 
 export default function AboutCard() {
   return (
     <div className="w-full h-fit p-1 m-auto sm:px-24 max-h-fit relative">
-      <div className="flex flex-col items-start py-8 px-8 max-w-fit mx-auto my-0 rounded-xl space-y-2 sm:py-4 sm:flex sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6">
-        <img className="block mx-auto h-32 rounded-full sm:mx-0 sm:shrink-0" src={profilePhoto} alt="Profile face" />
-        <div className="text-center sm:text-left text-xl">
-          <div className="w-auto">
-            <p className="text-lime-50 font-normal font-mono mb-1 overflow-y-auto">
-              I'm Edenys Deniz González, a computer scientist based in Santa Clara, Cuba.
+      <div className="flex flex-col sm:justify-center py-8 px-8  sm:p-0 mx-auto my-0 rounded-xl space-y-2 sm:py-4 sm:flex sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="block mx-auto h-44 rounded-full sm:mx-0 sm:shrink-0"
+          src={profilePhoto}
+          alt="Profile face"
+        />
+        <div className="text-left text-xl">
+          <div className="flex flex-col w-auto font-mono text-lime-50 overflow-y-auto gap-4">
+            <p className="sm:text-left text-center">
+              <strong className="text-amber-300"> Computer Scientist</strong>
             </p>
-            <p className="text-lime-50 font-normal font-mono mb-1 overflow-y-auto">
-              I have a career in competitive programming of more than eight years,
-              I am currently working as a frontend developer.
+            <p>
+              <strong className="text-amber-300">5 years</strong> of experience
+              working in frontend positions
             </p>
-            <p className="text-lime-50 font-normal font-mono mb-1 overflow-y-auto">
-              My free time is usually spent with family and friends, or reading something on the Internet.
+            <p>
+              <strong className="text-amber-300"> 8 years</strong> career in
+              competitive programming.
             </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
