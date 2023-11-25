abe7f04b17db857db2b436e2ead7291f94ac70a8
Edenys Deniz González <holofist1@gmail.com>
2023-11-25 02:13:08 -0500

    feat: improve aligment on card layout

--- src/components/projects-section/ProjectGithubCardContent.js
+++ src/components/projects-section/ProjectGithubCardContent.js
@@ -8,13 +8,15 @@ const ProjectGithubCardContent = ({
   topics,
 }) => {
   return (
-    <div className={"h-full w-auto"}>
+    <div className={"flex flex-col h-full w-auto"}>
       <p className="text-2xl pt-1 font-sans font-medium">{title}</p>
       <p className="text-xl py-2 font-sans bg-semizinc font-light">
         {description}
       </p>
-      {languages ? <LanguagesCardComponent languages={languages} /> : null}
-      {topics ? <TopicsCardComponent topics={topics} /> : null}
+      <div className={"flex flex-col justify-end h-full w-full"}>
+        {languages ? <LanguagesCardComponent languages={languages} /> : null}
+        {topics ? <TopicsCardComponent topics={topics} /> : null}
+      </div>
     </div>
   );
 };
