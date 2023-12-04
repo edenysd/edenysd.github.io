93a7ac067358717a803f7743c01efe0591c27b48
Edenys Deniz González <holofist1@gmail.com>
2023-12-04 16:05:50 -0500

    feat: improve accesibility

--- src/components/about-section/AboutCard.js
+++ src/components/about-section/AboutCard.js
@@ -3,23 +3,25 @@ import profilePhoto from "../../media/sections/about-section/profile-photo.jpg";
 export default function AboutCard() {
   return (
     <div className="w-full h-fit p-1 m-auto sm:px-24 max-h-fit relative">
-      <div className="flex flex-col sm:justify-center py-8 px-8  sm:p-0 mx-auto my-0 rounded-xl space-y-2 sm:py-4 sm:flex sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6">
+      <div className="flex flex-col sm:justify-center py-8 px-8 sm:p-0 mx-auto my-0 rounded-xl space-y-2 sm:py-4 sm:flex sm:flex-row sm:items-center sm:space-y-0 sm:space-x-6 backdrop-blur-sm">
         <img
           className="block mx-auto h-44 rounded-full sm:mx-0 sm:shrink-0"
           src={profilePhoto}
           alt="Profile face"
         />
         <div className="text-left text-xl">
-          <div className="flex flex-col w-auto font-mono text-lime-50 overflow-y-auto gap-4">
+          <div className="flex flex-col w-auto font-mono text-lime-50 overflow-y-auto gap-4 pl-2">
             <p className="sm:text-left text-center">
-              <strong className="text-amber-300"> Computer Scientist</strong>
+              <span className="text-white font-semibold">
+                Computer Scientist
+              </span>
             </p>
             <p>
-              <strong className="text-amber-300">5 years</strong> of experience
+              <span className="text-lime-200 glow">5 years</span> of experience
               working in frontend positions
             </p>
             <p>
-              <strong className="text-amber-300"> 8 years</strong> career in
+              <span className="text-lime-200 glow"> 8 years</span> career in
               competitive programming.
             </p>
           </div>
