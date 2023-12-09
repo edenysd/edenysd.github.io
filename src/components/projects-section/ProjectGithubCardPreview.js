c833da0bfc5e91c9a3a5f5cb425d3e04afcc8b1b
Edenys Deniz González <holofist1@gmail.com>
2023-12-09 01:35:39 -0500

    feat: squared images on project preview

--- src/components/projects-section/ProjectGithubCardPreview.js
+++ src/components/projects-section/ProjectGithubCardPreview.js
@@ -1,36 +1,48 @@
 import gsap from "gsap/all";
 import { random } from "gsap/gsap-core";
-import React from "react"
+import React from "react";
 
 const ProjectGithubCardPreview = ({ media }) => {
-    const [currentIndex, setIndex] = React.useState(0)
-    const imgRef = React.useRef(null)
-    React.useEffect(() => {
-        const timeline = gsap.timeline({ repeat: "1000000", repeatDelay: random(10, 20), yoyo: true })
-        timeline.add(
-            gsap.to(imgRef.current, {
-                duration: 0.2,
-                opacity: 0,
-            }))
-        timeline.add(
-            () => setIndex(
-                (oldIndex) => {
-                    return (oldIndex + 1) % (media.length)
-                }
-            )
-        )
-        timeline.add(
-            gsap.to(imgRef.current, {
-                duration: 0.2,
-                opacity: "1",
-            }))
-        return () => {
-            timeline.clear()
-        }
-    }, [media.length])
-    return (
-        <img ref={imgRef} className={"flex h-60  w-full"} style={{ objectFit: "contain" }} src={media ? media[currentIndex] : ""} alt={"examples"} />
-    )
-}
+  const [currentIndex, setIndex] = React.useState(0);
+  const imgRef = React.useRef(null);
+  React.useEffect(() => {
+    const timeline = gsap.timeline({
+      repeat: "1000000",
+      repeatDelay: random(10, 20),
+      yoyo: true,
+    });
+    timeline.add(
+      gsap.to(imgRef.current, {
+        duration: 0.2,
+        opacity: 0,
+      })
+    );
+    timeline.add(() =>
+      setIndex((oldIndex) => {
+        return (oldIndex + 1) % media.length;
+      })
+    );
+    timeline.add(
+      gsap.to(imgRef.current, {
+        duration: 0.2,
+        opacity: "1",
+      })
+    );
+    return () => {
+      timeline.clear();
+    };
+  }, [media.length]);
+  return (
+    <div style={{ width: "100%", paddingBottom: "100%", position: "relative" }}>
+      <img
+        style={{ objectFit: "cover", position: "absolute" }}
+        className={"h-full  w-full"}
+        ref={imgRef}
+        src={media ? media[currentIndex] : ""}
+        alt={"examples"}
+      />
+    </div>
+  );
+};
 
-export default ProjectGithubCardPreview
+export default ProjectGithubCardPreview;
