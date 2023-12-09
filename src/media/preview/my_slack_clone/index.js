3c56eda90adca7f065117a65952dcf0ea4759b07
Edenys Deniz González <holofist1@gmail.com>
2023-12-09 01:32:18 -0500

    feat: add slack clone multimedia

---
+++ src/media/preview/my_slack_clone/index.js
@@ -0,0 +1,7 @@
+import preview1 from "./preview1.png";
+import preview2 from "./preview2.png";
+import preview3 from "./preview3.png";
+import preview4 from "./preview4.png";
+
+const resources = [preview1, preview2, preview3, preview4];
+export default resources;
