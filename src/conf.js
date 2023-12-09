9c7cf5f6d47f42fca54772aa0f999637c878a9f7
Edenys Deniz González <holofist1@gmail.com>
2023-12-09 01:33:32 -0500

    content: add slack project content

--- src/conf.js
+++ src/conf.js
@@ -1,24 +1,43 @@
-import portfolioMedia from "./media/preview/portfolio"
-import gatoRatonMedia from "./media/preview/gato_raton"
+import portfolioMedia from "./media/preview/portfolio";
+import gatoRatonMedia from "./media/preview/gato_raton";
+import slackCloneMedia from "./media/preview/my_slack_clone";
 export const projects = [
-    {
-        title: "Edenys' Portfolio",
-        description: "Here is the project for this portfolio in case you have any questions or simply do you want to see how it works.",
-        media: portfolioMedia,
-        github: {
-            src: "https://github.com/edenysd/edenysd.github.io",
-            owner: "edenysd",
-            repo: "edenysd.github.io"
-        }
+  {
+    title: "My Slack Clone",
+    description: `<strong>Slack Clone SPA</strong>.
+       With avatars, conversations, private messages and channels.
+
+       Log in and see the demo.
+       `,
+    media: slackCloneMedia,
+    github: {
+      src: "https://github.com/edenysd/my-slack-clone",
+      owner: "edenysd",
+      repo: "my-slack-clone",
     },
-    {
-        title: "El Gato y el Raton",
-        description: "A beautiful game with some interesting things. The entire game is rendered on a single canvas using good programming practices to control game state and transicions. The only utilities used are the android canvas and the system event provider.",
-        media: gatoRatonMedia,
-        github: {
-            src: "https://github.com/edenysd/El-Gato-y-el-Raton",
-            owner: "edenysd",
-            repo: "El-Gato-y-el-Raton"
-        }
-    }
-]
+  },
+  {
+    title: "Edenys' Portfolio",
+    description: `I truly loved working on this project.
+        gsap3 is great, but the process of building your
+        visuals by hand is simply pure joy.
+      `,
+    media: portfolioMedia,
+    github: {
+      src: "https://github.com/edenysd/edenysd.github.io",
+      owner: "edenysd",
+      repo: "edenysd.github.io",
+    },
+  },
+  {
+    title: "El Gato y el Raton",
+    description:
+      "A beautiful game with some interesting things. The entire game is rendered on a single canvas using good programming practices to control game state and transitions. The only utilities used are the android canvas and the system event provider.",
+    media: gatoRatonMedia,
+    github: {
+      src: "https://github.com/edenysd/El-Gato-y-el-Raton",
+      owner: "edenysd",
+      repo: "El-Gato-y-el-Raton",
+    },
+  },
+];
