8a55601a1e9c7751dab63bf07385a2d57ec83f5a
Edenys Deniz González <holofist1@gmail.com>
2024-02-18 17:45:44 -0500

    feat: rework pointer events

--- src/components/about-section/AboutCard.js
+++ src/components/about-section/AboutCard.js
@@ -2,7 +2,7 @@ import profilePhoto from "../../media/sections/about-section/profile-photo.jpg";
 
 export default function AboutCard() {
   return (
-    <div className="w-full h-fit p-1 m-auto sm:px-24 max-h-fit relative">
+    <div className="w-full h-fit p-1 m-auto sm:px-24 max-h-fit relative pointer-events-none">
       <div
         className="w-fit flex flex-col md:justify-center py-8 px-8 md:p-8 mx-auto my-0 rounded-xl space-y-2 md:py-4
         md:flex-row md:items-center md:space-y-0 md:space-x-6 backdrop-blur-sm"
@@ -12,7 +12,7 @@ export default function AboutCard() {
           src={profilePhoto}
           alt="Profile face"
         />
-        <div className="text-left text-xl">
+        <div className="text-left text-xl pointer-events-auto">
           <div className="flex flex-col w-auto font-mono text-lime-50 overflow-y-auto gap-4 pl-2">
             <p className="md:text-left text-center">
               <span className="text-white font-semibold">
