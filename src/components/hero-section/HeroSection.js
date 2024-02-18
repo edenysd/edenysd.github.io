0b7f23d1dfc254e184ca246aca829619984e0f67
Edenys Deniz González <holofist1@gmail.com>
2024-02-18 18:54:22 -0500

    feat: use black background on hero section

--- src/components/hero-section/HeroSection.js
+++ src/components/hero-section/HeroSection.js
@@ -1,110 +1,57 @@
 import React from "react";
 
-import HeroPerspectiveWrapper from "./HeroPerspectiveWrapper";
-import background from "../../media/sections/hero-section/background-hero-reduced.jpg";
-
-const HERO_TEXT_SHADOW_X = 2,
-  HERO_TEXT_SHADOW_Y = 2;
-
 const HeroSection = () => {
-  const backgroundRef = React.useRef(null);
-  const presentationRef = React.useRef(null);
-
-  const processMouseMoveEvent = React.useCallback((event, topOffset) => {
-    const halfWindowH = window.innerHeight * 0.5,
-      halfWindowW = window.innerWidth * 0.5;
-
-    const xDeltaToCenter = event.pageX - halfWindowW,
-      yDeltaToCenter = halfWindowH - event.pageY - topOffset;
-
-    const {
-      x: presentationX,
-      width: presentationWidth,
-      height: presentationHeight,
-      y: presentationY,
-    } = presentationRef.current.getBoundingClientRect();
-    const xDeltaToPresentation =
-        event.pageX - presentationX - presentationWidth / 2,
-      yDeltaToPresentation =
-        event.pageY - presentationY - presentationHeight / 2;
-
-    backgroundRef.current &&
-      (backgroundRef.current.style.filter = `hue-rotate(${
-        (Math.atan2(yDeltaToCenter, xDeltaToCenter) / Math.PI) * 180
-      }deg)`);
-    presentationRef.current.style.filter = /*`hue-rotate(${Math.atan2(yDeltaToCenter, xDeltaToCenter) / Math.PI * 180}deg)`+*/ `drop-shadow(${
-      HERO_TEXT_SHADOW_X * (xDeltaToPresentation / halfWindowW)
-    }px ${
-      HERO_TEXT_SHADOW_Y * (yDeltaToPresentation / halfWindowH)
-    }px 2px #ffffff55)`;
-  }, []);
-
   return (
-    <HeroPerspectiveWrapper processMouseMoveEvent={processMouseMoveEvent}>
-      <div
-        className="absolute w-full dynamic-h-screen align-middle flex p-0 sm:p-16 flex-col lg:flex-row justify-center lg:justify-between"
-        style={{ transform: "translateZ(50px)" }}
-      >
-        <div className="w-full flex py-16 sm:p-16 flex-col lg:flex-row justify-center lg:justify-between">
-          <div
-            className="flex pb-3 items-end sm:items-center"
-            style={{
-              transform: "translateZ(20px)",
-            }}
-          >
-            <div className="w-full" ref={presentationRef}>
-              <div
-                className="flex flex-col lg:text-left"
+    <div
+      id="hero-section"
+      className="w-full h-screen align-middle flex p-0 sm:p-16 flex-col lg:flex-row justify-center lg:justify-between bg-black"
+    >
+      <div className="w-full flex py-16 sm:p-16 flex-col lg:flex-row justify-center lg:justify-between">
+        <div
+          className="flex pb-3 items-end sm:items-center"
+          style={{
+            transform: "translateZ(20px)",
+          }}
+        >
+          <div className="w-full">
+            <div
+              className="flex flex-col lg:text-left select-none"
+              style={{
+                color: "rgb(247 247 247)",
+                fontFamily: "Literata",
+                fontSize: "60px",
+                fontWeight: "400",
+                whiteSpace: "pre",
+              }}
+            >
+              <span
                 style={{
-                  color: "rgb(247 247 247)",
-                  fontFamily: "Literata",
-                  fontSize: "60px",
-                  fontWeight: "400",
-                  whiteSpace: "pre",
+                  paintOrder: "fill",
+                  fontWeight: "600",
+                  strokeWidth: "0px",
                 }}
               >
-                <span
-                  style={{
-                    paintOrder: "fill",
-                    fontWeight: "600",
-                    strokeWidth: "0px",
-                  }}
-                >
-                  Hi! I'm
-                </span>
-                <span style={{ fontWeight: "600", color: "rgb(164 237 185)" }}>
-                  Edenis Denis
-                </span>
-              </div>
+                Hi! I'm
+              </span>
+              <span
+                className="-rotate-12 sm:translate-x-9 glow-3"
+                style={{ fontWeight: "600", color: "rgb(164 237 185)" }}
+              >
+                Edenis Denis
+              </span>
             </div>
           </div>
-          <div
-            className="flex items-top  sm:items-center"
-            style={{
-              transform: "translateZ(11px)",
-            }}
-          >
-            {/* <Phrase className="w-full" /> */}
-          </div>
+        </div>
+        <div
+          className="flex items-top  sm:items-center"
+          style={{
+            transform: "translateZ(11px)",
+          }}
+        >
+          {/* <Phrase className="w-full" /> */}
         </div>
       </div>
-      <img
-        ref={backgroundRef}
-        src={background}
-        style={{
-          position: "relative",
-          height: "105vh",
-          width: "110vw",
-          maxHeight: "110vh",
-          maxWidth: "110vw",
-          left: "-5%",
-          top: "-5%",
-          objectFit: "cover",
-          transform: "translateZ(10px)",
-        }}
-        alt={""}
-      />
-    </HeroPerspectiveWrapper>
+    </div>
   );
 };
 
