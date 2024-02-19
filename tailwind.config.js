efcdb58f2e380479a17535bf23b9901d0b757817
Edenys Deniz González <holofist1@gmail.com>
2024-02-18 21:38:38 -0500

    feat: migrate to vite from CRA
    - add react types
    - add eslint + plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
-const colors = require("tailwindcss/colors")
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
       ...colors,
       semizinc: "rgba(150,150,150,0.1)",
-      transparentBlack: "rgba(0,0,0,0.3)"
+      transparentBlack: "rgba(0,0,0,0.3)",
     },
   },
-  plugins: [
-  ],
-}
+  plugins: [],
+};
