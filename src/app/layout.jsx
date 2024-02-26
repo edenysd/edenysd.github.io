ca9ee459b723dacfb3e64313dc380411499c757e
Edenys Deniz González <holofist1@gmail.com>
2024-02-25 20:23:47 -0500

    fix: set metadataBase field

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: new URL("https://edenysd.github.io/"),
   author: "Edenys Denis González",
   title: `Edenys' Portfolio`,
+  openGraph: {
+    images: "/og-image.png",
+  },
   description:
     "Edenys Denis González Portfolio. Hi! I am Computer Scientist. I talk mainly about web development, machine learning, data structures and algorithms.",
 };
